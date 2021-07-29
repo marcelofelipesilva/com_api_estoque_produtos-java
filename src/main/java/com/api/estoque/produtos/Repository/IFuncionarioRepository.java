64841221f83714ad4b81b167cfb5fe1c8661878a
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-07-29 18:47:15 -0300

    adicionado funcionario service.

--- src/main/java/com/api/estoque/produtos/Repository/IFuncionarioRepository.java
+++ src/main/java/com/api/estoque/produtos/Repository/IFuncionarioRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.estoque.produtos.Models.Funcionario;
 
-public interface IFuncionarioRepsository extends JpaRepository<Funcionario,Long> {
+public interface IFuncionarioRepository extends JpaRepository<Funcionario,Long> {
 
 }
--- src/main/java/com/api/estoque/produtos/Service/FuncionarioService.java
+++ src/main/java/com/api/estoque/produtos/Service/FuncionarioService.java
@@ -1,5 +1,39 @@
 package com.api.estoque.produtos.Service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.api.estoque.produtos.Models.Funcionario;
+import com.api.estoque.produtos.Repository.*;
+
 public class FuncionarioService {
+	
+	@Autowired
+	private IFuncionarioRepository funcionario;
+
+	public List<Funcionario> findAll() {
+		return funcionario.findAll();
+	}
+
+	public <S extends Funcionario> S saveAndFlush(S entity) {
+
+		return funcionario.saveAndFlush(entity);
+	}
+
+	public Funcionario getById(Long id) {
+		return funcionario.getById(id);
+	}
+	
+	public Funcionario save(Funcionario entity) {
+		funcionario.save(entity);
+		Long id = entity.getID();
+		return funcionario.getById(id);
+		
+	}
+
+	public void deleteById(Long id) {
+		funcionario.deleteById(id);
+	}
 
 }
