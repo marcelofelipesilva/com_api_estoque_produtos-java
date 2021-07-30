468e3a81012f645e6eece15ff7079d7b8e20bb0a
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-07-30 16:51:56 -0300

    adicionado fornecedor service

--- src/main/java/com/api/estoque/produtos/Service/FornecedorService.java
+++ src/main/java/com/api/estoque/produtos/Service/FornecedorService.java
@@ -1,5 +1,40 @@
 package com.api.estoque.produtos.Service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.api.estoque.produtos.Models.Fornecedor;
+import com.api.estoque.produtos.Repository.IFornecedorRepository;
+
 public class FornecedorService {
 
+	
+	
+	@Autowired
+	private IFornecedorRepository fornecedor;
+
+	public List<Fornecedor> findAll() {
+		return fornecedor.findAll();
+	}
+
+	public <S extends Fornecedor> S saveAndFlush(S entity) {
+
+		return fornecedor.saveAndFlush(entity);
+	}
+
+	public Fornecedor getById(Long id) {
+		return fornecedor.getById(id);
+	}
+	
+	public Fornecedor save(Fornecedor entity) {
+		fornecedor.save(entity);
+		Long id = entity.getID();
+		return fornecedor.getById(id);
+		
+	}
+
+	public void deleteById(Long id) {
+		fornecedor.deleteById(id);
+	}
 }
